cdafcd91e0fc3f16d721d2c628b8325b5ad5b833
Ellis Breen <ellis.breen@couchbase.com>
2019-02-19 16:53:45 +0000

    Preliminary Hello World

---
+++ modules/hello-world/examples/hello_world_t.py
@@ -0,0 +1,96 @@
+
+"""
+[source,python]
+----
+"""
+#tag::intro[]
+from couchbase.v3 import Cluster
+from couchbase.cluster import PasswordAuthenticator
+cluster = Cluster('couchbase://localhost')
+authenticator = PasswordAuthenticator('username', 'password')
+cluster.authenticate(authenticator)
+cb = cluster.bucket('bucket-name')
+cb_coll=cb.default_collection()
+cb_coll.upsert('u:king_arthur', {'name': 'Arthur', 'email': 'kingarthur@couchbase.com', 'interests': ['Holy Grail', 'African Swallows']})
+# OperationResult<RC=0x0, Key=u'u:king_arthur', CAS=0xb1da029b0000>
+
+print(cb_coll.get('u:king_arthur').contentAs[str])
+# {u'interests': [u'Holy Grail', u'African Swallows'], u'name': u'Arthur', u'email': u'kingarthur@couchbase.com'}
+
+## The CREATE PRIMARY INDEX step is only needed the first time you run this script
+cluster.query('CREATE PRIMARY INDEX ON bucket-name')
+
+from couchbase.n1ql import N1QLQuery
+row_iter = cluster.query(N1QLQuery('SELECT name FROM bucket-name WHERE ' + \
+                                   '$1 IN interests', 'African Swallows'))
+for row in row_iter: print(row)
+# {u'name': u'Arthur'}
+#end::intro[]
+"""
+----
+
+== Connecting
+
+To connect to a Couchbase bucket, you must use Couchbase _Role-Based Access Control_ (RBAC).
+This is fully described in the section xref:6.0@server:security:security-authorization.adoc[Authorization].
+An _authenticator_, containing username and password, should be defined, and then passed to the cluster.
+Following successful authentication, the bucket can be opened.
+
+[source,python]
+----
+"""
+#tag::connecting[]
+from couchbase.v3 import Cluster
+from couchbase.cluster import PasswordAuthenticator
+cluster = Cluster('couchbase://localhost')
+authenticator = PasswordAuthenticator('username', 'password')
+cluster.authenticate(authenticator)
+bucket = cluster.bucket('bucket-name')
+coll = bucket.default_collection()
+#end::connecting[]
+"""
+----
+
+Once defined, the authenticator can be passed to other clusters, as appropriate.
+
+See xref:managing-connections.adoc[Managing Connections Using the Python SDK with Couchbase Server] for more connection options and details about the connection string.
+
+== Document Operations
+
+xref:core-operations.adoc[Document operations], such as storing and retrieving documents, can be done using simple methods on the [.api]`Bucket` class such as [.api]`Bucket.get` and [.api]`Bucket.upsert`.
+Simply pass the key (and value, if applicable) to the relevant methods.
+
+[source,python]
+----
+"""
+#tag::docopsget[]
+rv = coll.get('document-id')
+print(rv.content)
+#end::docopsget[]
+"""
+----
+
+[source,python]
+----
+"""
+#tag::docopsupsert[]
+coll.upsert('document-id', {'application': 'data'})
+#tag::docopsupsert[]
+"""----
+
+== N1QL Queries
+
+Couchbase N1QL queries are performed by creating a [.api]`N1QLQuery` object and passing that to the [.api]`Bucket.n1ql_query()` method:
+
+[source,python]
+----
+"""
+#tag::n1ql[]
+from couchbase.n1ql import N1QLQuery
+query = N1QLQuery("""SELECT airportname, city, country FROM `travel-sample` """
+                  """WHERE type="airport" AND city=$my_city""", my_city="Reno")
+for row in cluster.query(query):
+    print(row)
+#end::n1ql[]
+"""----
+"""
