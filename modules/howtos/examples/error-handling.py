d58620b4373367a40c3e728284598071149c2328
Ellis Breen <ellis.breen@couchbase.com>
2019-06-13 16:03:34 +0100

    Error handling except async stuff (to be done in a later Alpha)

--- modules/howtos/examples/error-handling.py
+++ modules/howtos/examples/error-handling.py
@@ -1,4 +1,4 @@
-
+"""
 == How the SDK Handles Errors [EH1]
 == Handling Errors
 === Failing [EH2]
@@ -37,7 +37,15 @@ Go Errors page up at https://docs.couchbase.com/go-sdk/2.0/howtos/error-handling
 ////
 Meta: version 1 goes from EH1-EH6
 ////
-
+"""
+from couchbase import Collection, GetResult, KeyNotFoundException, Durability, KeyExistsException, PersistTo, \
+    ReplicateTo, TimeoutException
+import couchbase.exceptions
+from couchbase.collection import Collection
+from couchbase.cluster import Cluster
+import logging
+collection=Cluster().bucket("fred").default_collection()
+"""
 = Handling Errors
 :navtitle: Handling Errors
 :page-topic-type: howto
@@ -70,17 +78,54 @@ While most of the time you want more sophisticated error handling strategies, so
 It makes no sense for some errors to be retried, either because they are not transient, or because you already tried everything to make it work and it still keeps failing.
 If containment is not able to handle the error, then it needs to propagate up to a parent component that can handle it.
 
+In the asynchronous case, errors are events like any other for your subscribers.
+Once an error occurs, your `Subscriber` is notified in the method `onError(Throwable)`, and you can handle it the way you want to. Note that by `Observable` contract, after the `onError` event, no more `onNext` events will happen.
+
+[source,python]
+----
+"""
+
+Observable
+.error(new Exception("I'm failing"))
+.subscribe(new Subscriber<Object>() {
+@Override
+public void onCompleted() {
+}
+
+@Override
+public void onError(Throwable e) {
+System.err.println("Got Error: " + e);
+}
+
+@Override
+public void onNext(Object o) {
+}
+});
+"""
 ----
 
 For synchronous programs, every error is converted into an Exception and thrown so that you can use regular `try`/`catch` semantics.
 
+[source,java]
+----
+"""
+try:
+Object data = Observable.error(new Exception("I'm failing"))
+.toBlocking()
+.single();
+} catch(Exception ex) {
+System.err.println("Got Exception: " + ex);
+}
+"""
+----
+
 If you do not catch the Exception, it will bubble up:
 
-[source,python]
+[source,java]
 ----
 Exception in thread "main" java.lang.RuntimeException: java.lang.Exception: I'm failing
- at rx.observables.BlockingObservable.blockForSingle(BlockingObservable.java:482)
- at rx.observables.BlockingObservable.single(BlockingObservable.java:349)
+at rx.observables.BlockingObservable.blockForSingle(BlockingObservable.java:482)
+at rx.observables.BlockingObservable.single(BlockingObservable.java:349)
 ----
 === Logging [EH3]
 It is always important to log errors, but even more so in the case of reactive applications. Because of the event driven nature, stack traces get harder to look at, and caller context is sometimes lost.
@@ -88,7 +133,9 @@ It is always important to log errors, but even more so in the case of reactive a
 # SDK-specific text in each case
 # Note that Logging has its own page.
 
+
 # recommendation of good practice ADMONITION link
+"""
 === Retry [EH4]
 
 Transient errors -- such as those caused by resource starvation -- are best tackled with one of the following retry strategies:
@@ -101,8 +148,57 @@ Retry with a random delay.
 
 [source,python]
 ----
-include::example$error-handling.py[tag=handle_retryable]
+"""
 
+#tag::handle_retryable[]
+# This an example of error handling for idempotent operations (such as the full-doc op seen here).
+
+def change_email(collection,  # type: Collection
+                 maxRetries  # type: int
+                 ):
+    try:
+        result = collection.get("doc_id")  # type: GetResult
+
+        if not result:
+            raise couchbase.exceptions.KeyNotFoundException()
+        else:
+            content = result.content
+
+        content["email"]="john.smith@couchbase.com"
+
+        collection.replace("doc_id", content);
+    except couchbase.exceptions.CouchbaseError as err:
+        # isRetryable will be true for transient errors, such as a CAS mismatch (indicating
+        # another agent concurrently modified the document), or a temporary failure (indicating
+        # the server is temporarily unavailable or overloaded).  The operation may or may not
+        # have been written, but since it is idempotent we can simply retry it.
+        if err.is_retryable:
+            if maxRetries > 0:
+                logging.info("Retrying operation on retryable err " + err)
+            change_email(collection, maxRetries - 1);
+        else:
+            # Errors can be transient but still exceed our SLA.
+            logging.error("Too many attempts, aborting on err " + err)
+            raise
+
+        # If the err is not isRetryable, there is perhaps a more permanent or serious error,
+        # such as a network failure.
+        else:
+            logging.error("Aborting operation on err " + err);
+            raise
+
+
+MAX_RETRIES=5
+try:
+    change_email(collection, MAX_RETRIES)
+except RuntimeError as err:
+    # What to do here is highly application dependent.  Options could include:
+    # - Returning a "please try again later" error back to the end-user (if any)
+    # - Logging it for manual human review, and possible follow-up with the end-user (if any)
+    logging.error("Failed to change email")
+#end::handle_retryable[]
+
+"""
 ----
 
 === Fallback
@@ -121,44 +217,91 @@ There are some errors which can be surfaced from across all of the SDK services.
 The KV Service exposes several common errors that can be encountered - both during development, and to be handled by the production app. Here we will cover some of the most common errors.
 
 === Doc does not exist [EH5]
-[source,python]
-----
-include::example$error-handling.py[tag=KeyNotFoundException]
+"""
+#tag::KeyNotFoundException[]
 
+try:
+    collection.replace("my-key", {})
+except KeyNotFoundException:
+    # key does not exist
+    pass
+#end::KeyNotFoundException[]
 
-=== Doc already exists
-[source,python]
-----
-include::example$error-handling.py[tag=KeyExistsException]
-
+#tag::KeyExistsException[]
+try:
+    collection.insert("my-key",{})
+except KeyExistsException:
+    # key already exists
+    pass
+#end::KeyExistsException[]
+"""
 === Doc too large
 RequestTooBigException
 
 === CAS Mismatch [EH6]
-[source,python]
-----
-include::example$error-handling.py[tag=CASMismatchException]
+"""
+#tag::CASMismatchException[]
+try:
+    result = collection.get("my-key")
+    collection.replace("my-key", {}, cas = result.cas)
+except couchbase.exceptions.CASMismatchException:
+    # the CAS value has changed
+    pass
+#end::CASMismatchException[]
+"""
 === Durability ambiguous [EH7]
-[source,python]
-----
-include::example$error-handling.py[tag=DurabilitySyncWriteAmbiguousException]
-=== Durability invalid level [EH7(also)]
-[source,python]
-----
-include::example$error-handling.py[tag=DurabilityInvalidLevelException]
-=== No Cluster replicas configured [EH7(also)]
-[source,python]
-----
-include::example$error-handling.py[tag=ReplicaNotConfiguredException]
-=== Replicate to / persist to greater than replica count [EH8]
-[source,python]
-----
-include::example$error-handling.py[tag=DurabilityImpossibleException]
+"""
+#tag::DurabilitySyncWriteAmbiguousException[]
+try:
+    collection.upsert("my-key", {}, durability_level=Durability.PERSIST_TO_MAJORITY)
+except couchbase.exceptions.DurabilitySyncWriteAmbiguousException:
+    # synchronous durability request has not completed, it is unknown whether the request met the durability requirements or not
+    pass
+#end::DurabilitySyncWriteAmbiguousException[]
 
+"""
+=== Durability invalid level [EH7(also)]
+"""
+#tag::DurabilityInvalidLevelException[]
+try:
+    collection.upsert("my-key", {}, durability_level=Durability.PERSIST_TO_MAJORITY)
+except couchbase.exceptions.DurabilityInvalidLevelException:
+    # cluster not able to meet durability requirements
+    pass
+#end::DurabilityInvalidLevelException[]
+"""
+=== No Cluster replicas configured [EH7(also)]
+"""
+#tag::ReplicaNotConfiguredException[]
+try:
+    collection.upsert("my-key", {}, persist_to=PersistTo.FOUR, ReplicateTo.THREE)
+except couchbase.exceptions.ReplicaNotConfiguredException:
+    # cluster doesn't have replicas configured
+    pass
+#end::ReplicaNotConfiguredException[]
+"""
+=== Replicate to / persist to greater than replica count [EH8]
+"""
+#tag::DurabilityImpossibleException[]
+try:
+    collection.upsert("my-key", {}, persist_to=PersistTo.FOUR, replicate_to=ReplicateTo.THREE)
+except couchbase.exceptions.DurabilityImpossibleException:
+    # cluster not able to meet durability requirements
+    pass
+#end::DurabilityImpossibleException[]
+
+"""
 === Timeout with replicate to / persist to requirements
-[source,python]
-----
-include::example$error-handling.py[tag=TimeoutException]
+"""
+#tag::TimeoutException[]
+try:
+    collection.upsert("my-key", {}, persist_to=PersistTo.FOUR, replicate_to=ReplicateTo.THREE)
+except TimeoutException:
+    # document may or may not have persisted to specified durability requirements
+    pass
+#end::TimeoutException[]
+"""
+
 == Query and Analytics Errors
 N1ql and Analytics either return results or an error. If there is an error then it exposed in the following way(s)...
 
@@ -200,3 +343,4 @@ We offer xref:collecting-information.adoc[recommendations and practical examples
 
 We have a xref:ref:exceptions.adoc[listing of error messages], with some pointers to what to do when you encounter them.
 
+"""
