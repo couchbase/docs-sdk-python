e798c28daa5dd76c622bac68f909e6dbcea296f6
blbryant <bl_bryant@yahoo.com>
2020-08-26 12:24:56 -0500

    rewrite sample code against travel-sample

--- modules/hello-world/examples/hello_world_t.py
+++ modules/hello-world/examples/hello_world_t.py
@@ -1,95 +1,83 @@
-
 """
 [source,python]
 ----
 """
-#tag::intro[]
+# tag::connect[]
 from couchbase.cluster import Cluster, ClusterOptions
 from couchbase_core.cluster import PasswordAuthenticator
+from couchbase.cluster import QueryOptions
 
-cluster = Cluster('couchbase://localhost', ClusterOptions(PasswordAuthenticator('username', 'password')))
-cb = cluster.bucket('bucket-name')
+cluster = Cluster('couchbase://hulk', ClusterOptions(
+  PasswordAuthenticator('Administrator', 'password')))
+# end::connect[]
+
+# tag::bucket[]
+# get a reference to our bucket
+cb = cluster.bucket('travel-sample')
+# end::bucket[]
+
+# tag::default-collection[]
+# get a reference to the default collection
 cb_coll = cb.default_collection()
-cb_coll.upsert('u:king_arthur',
-               {'name': 'Arthur', 'email': 'kingarthur@couchbase.com', 'interests': ['Holy Grail', 'African Swallows']})
-# OperationResult<RC=0x0, Key=u'u:king_arthur', CAS=0xb1da029b0000>
+# end::default-collection[]
 
-print(cb_coll.get('u:king_arthur').content_as[str])
-# {u'interests': [u'Holy Grail', u'African Swallows'], u'name': u'Arthur', u'email': u'kingarthur@couchbase.com'}
+# tag::test-doc[]
+airline = {
+  "type": "airline",
+  "id": 8091,
+  "callsign": "CBS",
+  "iata": None,
+  "icao": None,
+  "name": "Couchbase Airways",
+}
+# end::test-doc[]
 
-## The CREATE PRIMARY INDEX step is only needed the first time you run this script
-cluster.query_indexes().create_primary_index('bucket-name')
+# tag::upsert-func[]
+def upsertDocument(doc):
+  print("\nUpsert CAS: ")
+  try:
+    # key will equal: "airline_8091"
+    key = doc["type"] + "_" + str(doc["id"])
+    result = cb_coll.upsert(key, doc)
+    print(result.cas)
+  except Exception as e:
+    print(e)
+# end::upsert-func[]
 
-from couchbase.cluster import QueryOptions
-row_iter = cluster.query('SELECT name FROM bucket-name WHERE $1 IN interests', QueryOptions(positional_parameters=['African Swallows']))
-for row in row_iter: print(row)
-# {u'name': u'Arthur'}
-#end::intro[]
-"""
-----
+# tag::upsert-invoke[]
+upsertDocument(airline)
+# end::upsert-invoke[]
 
-== Connecting
+# tag::get-func[]
+# get document function
+def getAirlineByKey(key):
+  print("\nGet Result: ")
+  try:
+    result = cb_coll.get(key)
+    print(result.content_as[str])
+  except Exception as e:
+    print(e)
+# end::get-func[]
 
-To connect to a Couchbase bucket, you must use Couchbase _Role-Based Access Control_ (RBAC).
-This is fully described in the section xref:6.0@server:security:security-authorization.adoc[Authorization].
-An _authenticator_, containing username and password, should be defined, and then passed to the Cluster constructor.
-Following successful authentication, the bucket can be opened.
+# tag::get-invoke[]
+# call get document function
+getAirlineByKey("airline_8091")
+#end::get-invoke[]
 
-[source,python]
-----
-"""
-#tag::connecting[]
-from couchbase.cluster import Cluster, ClusterOptions
-from couchbase_core.cluster import PasswordAuthenticator
-cluster = Cluster('couchbase://localhost', ClusterOptions(PasswordAuthenticator('username', 'password')))
-bucket = cluster.bucket('bucket-name')
-coll = bucket.default_collection()
-#end::connecting[]
-"""
-----
+#tag::lookup-func[]
+# query for new document by callsign
+def lookupByCallsign(cs):
+  print("\nLookup Result: ")
+  try:
+    sqlQuery = 'SELECT VALUE name FROM `travel-sample` WHERE type = "airline" AND callsign = $1' 
+    row_iter = cluster.query(
+      sqlQuery, 
+      QueryOptions(positional_parameters=[cs]))
+    for row in row_iter: print(row)
+  except Exception as e:
+    print(e)
+# end::lookup-func[]
 
-Once defined, the authenticator can be passed to other clusters, as appropriate.
-
-See xref:managing-connections.adoc[Managing Connections Using the Python SDK with Couchbase Server] for more connection options and details about the connection string.
-
-== Document Operations
-
-xref:core-operations.adoc[Document operations], such as storing and retrieving documents, can be done using simple methods on the [.api]`Bucket` class such as [.api]`Bucket.get` and [.api]`Bucket.upsert`.
-Simply pass the key (and value, if applicable) to the relevant methods.
-
-[source,python]
-----
-"""
-#tag::docopsget[]
-rv = coll.get('document-id')
-print(rv.content)
-#end::docopsget[]
-"""
-----
-
-[source,python]
-----
-"""
-#tag::docopsupsert[]
-coll.upsert('document-id', {'application': 'data'})
-#end::docopsupsert[]
-"""----
-
-== N1QL Queries
-
-Couchbase N1QL queries are performed by creating a [.api]`N1QLQuery` object and passing that to the [.api]`Bucket.n1ql_query()` method:
-
-[source,python]
-----
-"""
-#tag::n1ql[]
-from couchbase.cluster import QueryOptions
-
-query_result = cluster.query("""SELECT airportname, city, country FROM `travel-sample` """
-                             """WHERE type="airport" AND city=$my_city""",
-                             QueryOptions(named_parameters={'my_city': "Reno"}))
-for row in query_result:
-    print(row)
-#end::n1ql[]
-"""----
-"""
+# tag::lookup-invoke[]
+lookupByCallsign("CBS")
+# end::lookup-invoke[]
