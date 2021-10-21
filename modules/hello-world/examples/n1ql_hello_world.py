c0129bd5fa68c29379612db3c04c7e1bfdaa277d
Maria Shodunke <maria-robobug@users.noreply.github.com>
2021-10-21 15:06:40 +0100

    Shorten Iterate comment as suggested.
    Co-authored-by: Simon Dew <39966290+simon-dew@users.noreply.github.com>

--- modules/hello-world/examples/n1ql_hello_world.py
+++ modules/hello-world/examples/n1ql_hello_world.py
@@ -9,8 +9,7 @@ try:
     # Call the query() function on the cluster object and store the result.
     result = cluster.query("SELECT \"Hello World\" as greeting")
 
-    # The result object will include an array of rows found.
-    # We iterate over the rows to access our result data and print it to the terminal.
+    # Iterate over the rows to access result data and print to the terminal.
     for row in result.rows():
         print(row)
     # end::n1ql-query[]
