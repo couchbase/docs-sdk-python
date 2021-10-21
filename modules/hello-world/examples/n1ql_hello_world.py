4871257c0aee939658f18675bdd2a0848ae7b54b
Maria Shodunke <mariashow2001@gmail.com>
2021-10-21 10:22:18 +0100

    Add some comments for Server re-org sample

--- modules/hello-world/examples/n1ql_hello_world.py
+++ modules/hello-world/examples/n1ql_hello_world.py
@@ -6,7 +6,11 @@ cluster = Cluster("couchbase://localhost",
 
 try:
     # tag::n1ql-query[]
+    # To execute the query, we call the query() function on our cluster object and store the result.
     result = cluster.query("SELECT \"Hello World\" as greeting")
+
+    # The result object will include an array of rows found.
+    # We iterate over the rows to access our result data and print it to the terminal.
     for row in result.rows():
         print(row)
     # end::n1ql-query[]
