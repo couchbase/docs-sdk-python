c70f4731e93f2c67f08c6c7c41f19ba17f377fa6
Jared Casey <jared.casey@couchbase.com>
2022-04-11 13:18:06 -0500

    update cloud example to show use of kv_timeout option

--- modules/hello-world/examples/cloud.py
+++ modules/hello-world/examples/cloud.py
@@ -1,7 +1,14 @@
 # tag::cloud_connect[]
-from couchbase.cluster import Cluster, ClusterOptions
-from couchbase.cluster import PasswordAuthenticator
-from couchbase.cluster import QueryOptions
+from datetime import timedelta
+
+# needed for any cluster connection
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+
+# needed for options -- cluster, timeout, SQL++ (N1QL) query, etc.
+from couchbase.options import (ClusterOptions,
+                               ClusterTimeoutOptions,
+                               QueryOptions)
 
 # Update this to your cluster
 endpoint = "--your-instance--.dp.cloud.couchbase.com"
@@ -11,28 +18,94 @@ bucket_name = "bucketname"
 cert_path = "path/to/certificate"
 # User Input ends here.
 
-# Initialize the Connection
-cluster = Cluster("couchbases://{}".format(endpoint), ClusterOptions(
-    PasswordAuthenticator(username, password, cert_path=cert_path)))
-cb = cluster.bucket(bucket_name)
-cb_coll = cb.default_collection()
+# Connect options - authentication
+auth = PasswordAuthenticator(username, password, cert_path=cert_path)
+# Connect options - global timeout opts
+timeout_opts = ClusterTimeoutOptions(kv_timeout=timedelta(seconds=10))
 
-# Create a N1QL Primary Index (but ignore if it exists)
-cluster.query_indexes().create_primary_index(
-    bucket_name, ignore_if_exists=True)
+# get a reference to our cluster
+cluster = Cluster('couchbases://{}'.format(endpoint),
+                  ClusterOptions(auth, timeout_options=timeout_opts))
+# end::connect[]
 
-# Store a Document
-cb_coll.upsert("u:king_arthur", {
-               "name": "Arthur", "email": "kingarthur@couchbase.com", "interests": ["Holy Grail", "African Swallows"]})
+# tag::bucket[]
+# get a reference to our bucket
+cb = cluster.bucket('travel-sample')
+# end::bucket[]
 
-# Load the Document and print it
-print(cb_coll.get("u:king_arthur").content_as[str])
+# tag::collection[]
+cb_coll = cb.scope("inventory").collection("airline")
+# end::collection[]
 
-# Perform a N1QL Query
-row_iter = cluster.query("SELECT cbc.* FROM {} cbc WHERE $1 IN cbc.interests".format(
-    bucket_name), QueryOptions(positional_parameters=["African Swallows"]))
+# tag::default-collection[]
+# get a reference to the default collection, required for older Couchbase server versions
+cb_coll_default = cb.default_collection()
+# end::default-collection[]
 
-# Print each found Row
-for row in row_iter.rows():
-    print("Found row: {}".format(row))
-# end::cloud_connect[]
+# tag::upsert-func[]
+
+
+def upsert_document(doc):
+    print("\nUpsert CAS: ")
+    try:
+        # key will equal: "airline_8091"
+        key = doc["type"] + "_" + str(doc["id"])
+        result = cb_coll.upsert(key, doc)
+        print(result.cas)
+    except Exception as e:
+        print(e)
+# end::upsert-func[]
+
+# tag::get-func[]
+# get document function
+
+
+def get_airline_by_key(key):
+    print("\nGet Result: ")
+    try:
+        result = cb_coll.get(key)
+        print(result.content_as[str])
+    except Exception as e:
+        print(e)
+# end::get-func[]
+
+# tag::lookup-func[]
+# query for new document by callsign
+
+
+def lookup_by_callsign(cs):
+    print("\nLookup Result: ")
+    try:
+        sql_query = 'SELECT VALUE name FROM `travel-sample`.inventory.airline WHERE callsign = $1'
+        row_iter = cluster.query(
+            sql_query,
+            QueryOptions(positional_parameters=[cs]))
+        for row in row_iter:
+            print(row)
+    except Exception as e:
+        print(e)
+# end::lookup-func[]
+
+
+# tag::test-doc[]
+airline = {
+    "type": "airline",
+    "id": 8091,
+    "callsign": "CBS",
+    "iata": None,
+    "icao": None,
+    "name": "Couchbase Airways",
+}
+# end::test-doc[]
+
+# tag::upsert-invoke[]
+upsert_document(airline)
+# end::upsert-invoke[]
+
+# tag::get-invoke[]
+get_airline_by_key("airline_8091")
+# end::get-invoke[]
+
+# tag::lookup-invoke[]
+lookup_by_callsign("CBS")
+# end::lookup-invoke[]
