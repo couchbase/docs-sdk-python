3114b013416722fb0e913f7f54b84b2f3dea573c
Jared Casey <jared.casey@couchbase.com>
2022-08-02 16:09:48 -0500

    PYCBC-1394:  Update logging and search examples

--- modules/howtos/examples/logging_example.py
+++ modules/howtos/examples/logging_example.py
@@ -1,21 +1,39 @@
-# TODO: This is currently not implemented in SDK 4.x.
-# Don't use this example for now, recommend setting the `PYCBC_LOG_LEVEL` env variable instead.
+# logging implement in 4.0.2
 # tag::logging[]
 import logging
-import sys
+import traceback
+from datetime import timedelta
 
 import couchbase
 from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster
-from couchbase.options import ClusterOptions
+from couchbase.diagnostics import ServiceType
+from couchbase.exceptions import CouchbaseException
+from couchbase.options import ClusterOptions, WaitUntilReadyOptions
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-couchbase.enable_logging()
+# output log messages to example.log
+logging.basicConfig(filename='example.log',
+                    filemode='w', 
+                    level=logging.DEBUG,
+                    format='%(levelname)s::%(asctime)s::%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger()
+couchbase.configure_logging(logger.name, level=logger.level)
 
 cluster = Cluster('couchbase://localhost',
                   ClusterOptions(PasswordAuthenticator("Administrator", "password")))
 
+cluster.wait_until_ready(timedelta(seconds=3),
+                         WaitUntilReadyOptions(service_types=[ServiceType.KeyValue, ServiceType.Query]))
+
+logger.info('Cluster ready.')
+
 bucket = cluster.bucket("travel-sample")
 coll = bucket.scope('inventory').collection('airline')
-coll.upsert('key', ['value'])
+try:
+    coll.get('not-a-key')
+except CouchbaseException:
+    logger.error(traceback.format_exc())
+
 # end::logging[]
