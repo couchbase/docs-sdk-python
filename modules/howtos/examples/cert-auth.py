fe67aab73ba1fb806678d1d877090f79c572790f
Jared Casey <jared.casey@couchbase.com>
2023-03-09 11:01:44 -0600

    DOC-10840: Update Python 4.x auth examples

--- modules/howtos/examples/cert-auth.py
+++ modules/howtos/examples/cert-auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 # tag::auth[]
-from couchbase.cluster import Cluster, ClusterOptions
-from couchbase_core.cluster import CertAuthenticator
+from couchbase.auth import CertificateAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.options import ClusterOptions
+
 import os.path
 
 # point to certificates, keys, and trust stores;
@@ -10,11 +12,9 @@ import os.path
 # the script in etc/x509-cert will generate these
 clientdir = "etc/x509-cert/SSLCA/clientdir"
 options = dict(cert_path=os.path.join(clientdir, "client.pem"),
-    trust_store_path=os.path.join(clientdir, "trust.pem"),
-    key_path=os.path.join(clientdir, "client.key"))
+               trust_store_path=os.path.join(clientdir, "trust.pem"),
+               key_path=os.path.join(clientdir, "client.key"))
 
-cluster = Cluster('couchbase://localhost', ClusterOptions(
-    CertAuthenticator(options)
-    )
-)
+opts = ClusterOptions(CertificateAuthenticator(**options))
+cluster = Cluster('couchbase://localhost', opts)
 # end::auth[]
