8d467d492817203b0f1e904188b970646ab4b977
Richard Smedley <rgsmedley@gmail.com>
2026-05-27 14:21:52 +0100

    Fix async connection to Couchbase cluster
    await

--- modules/devguide/examples/python/acouchbase_analytics_ops.py
+++ modules/devguide/examples/python/acouchbase_analytics_ops.py
@@ -6,7 +6,7 @@ from couchbase.exceptions import CouchbaseException
 
 
 async def get_couchbase():
-    cluster = AsyncCluster.connect(
+    cluster = await AsyncCluster.connect(
         "couchbase://your-ip",
         ClusterOptions(PasswordAuthenticator("Administrator", "password")))
     bucket = cluster.bucket("travel-sample")
