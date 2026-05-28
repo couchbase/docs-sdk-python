5c3075fd37d253b276dbc48027d37a7682e7764f
Jared Casey <jared.casey@couchbase.com>
2026-05-27 18:24:39 -0600

    Update management connections examples

--- modules/devguide/examples/python/managing_connections.py
+++ modules/devguide/examples/python/managing_connections.py
@@ -6,6 +6,7 @@
 #  - /etc/hosts (or C:\Windows\System32\Drivers\etc\hosts on Windows)
 #      with `127.0.0.1 node2.example.com`
 
+from acouchbase.cluster import AsyncCluster
 from couchbase.cluster import Cluster
 from couchbase.options import ClusterOptions
 from couchbase.auth import PasswordAuthenticator
@@ -38,65 +39,6 @@ class ManagingConnections(object):
       cluster = Cluster.connect("couchbase://node1.example.com,node2.example.com", ClusterOptions(PasswordAuthenticator("Administrator", "password")))
       #end::multinodeconnect[]
 
-    #
-    # def test_customenv(self):
-    #   #tag::customenv[]
-    #   env = ClusterEnvironment.builder()
-    #       .build();
-    #   # Customize client settings by calling methods on the builder
-    #
-    #   # Create a cluster using the environment's custom client settings.
-    #   cluster = Cluster.connect("couchbase://your-ip", ClusterOptions
-    #       .clusterOptions("username", "password")
-    #       .environment(env))
-    #
-    #   # Shut down gracefully. Shut down the environment
-    #   # after all associated clusters are disconnected.
-    #   cluster.disconnect()
-    #   env.shutdown()
-    #   #end::customenv[]
-    #
-    #
-    # def test_shareclusterenv(self):
-    #   #tag::shareclusterenvironment[]
-    #   env = ClusterEnvironment.builder()\
-    #       .timeoutConfig(TimeoutConfig.kvTimeout(Duration.ofSeconds(5)))\
-    #       .build()
-    #
-    #   clusterA = Cluster.connect(
-    #       "clusterA.example.com",
-    #       ClusterOptions("username", "password")
-    #           .environment(env));
-    #
-    #   clusterB = Cluster.connect(
-    #       "clusterB.example.com",
-    #       ClusterOptions("username", "password")
-    #           .environment(env));
-
-    # # ...
-    #
-    #   # For a graceful shutdown, disconnect from the clusters
-    #   # AND shut down the custom environment when then program ends.
-    #   clusterA.disconnect();
-    #   clusterB.disconnect();
-    #   env.shutdown();
-    #end::shareclusterenvironment[]
-
-#
-#     // todo use this example when beta 2 is released.
-# //    {
-# //      // #tag::seednodes[]
-# //      int customKvPort = 12345;
-# //      int customManagerPort = 23456
-# //      Set<SeedNode> seedNodes = new HashSet<>(Arrays.asList(
-# //          SeedNode.create("127.0.0.1",
-# //              Optional.of(customKvPort),
-# //              Optional.of(customManagerPort))))
-# //
-#
-# //      Cluster cluster = Cluster.connect(seedNodes, "username", "password")
-# //      // #end::customconnect[]
-# //    }
 
     def test_connectionstringparams(self):
       print("connectionstringparams")
@@ -105,52 +47,26 @@ class ManagingConnections(object):
           "couchbase://your-ip?compression=on&log_redaction=on", ClusterOptions(PasswordAuthenticator("Administrator", "password")))
       #end::connectionstringparams[]
 
-    def test_async(self):
-      print("blockingtoasync")
-      #tag::blockingtoasync[]
-      cluster = Cluster.connect("couchbase://your-ip", ClusterOptions(PasswordAuthenticator("Administrator", "password")))
-      bucket = cluster.bucket("travel-sample")
-
-      # Same API as Bucket, but completely async with asyncio Futures
-      from acouchbase.bucket import Bucket
-      async_bucket=Bucket("couchbase://your-ip/default")
-
-      cluster.disconnect()
-      #end::blockingtoasync[]
-
-      print("reactivecluster")
-      #tag::reactivecluster[]
-#     //  This sample, and the above one not used -
-#     //  see the source of https://docs.couchbase.com/python-sdk/current/howtos/managing-connections.html#async-apis 
-        
-      from acouchbase.bucket import Bucket
+    async def test_async(self):
+      print("asynccluster")
+      #tag::asynccluster[]
       cluster = await AsyncCluster.connect("couchbase://your-ip", ClusterOptions(PasswordAuthenticator("Administrator", "password")))
       bucket = cluster.bucket("travel-sample")
 
-      cluster.disconnect()
-      #end::reactivecluster[]
-
-      print("asynccluster")
-      #tag::asynccluster[]
-      cluster = Cluster.connect("couchbase://your-ip", ClusterOptions(PasswordAuthenticator("Administrator", "password")))
-      bucket = cluster.bucket("travel-sample")
-
-      # An async cluster's disconnect methods returns a CompletableFuture<Void>.
-      # The disconnection starts as soon as you call disconnect().
-      # The simplest way to wait for the disconnect to complete is to call `join()`.
-      cluster.disconnect().join()
+      # For a graceful shutdown, disconnect from the cluster when the program ends.
+      await cluster.close()
       #end::asynccluster[]
 
       print("tls")
       #tag::tls[]
-      cluster = Cluster.connect("couchbases://your-ip",ClusterOptions(PasswordAuthenticator("Administrator","password",cert_path="/path/to/cluster.crt")))
+      cluster = Cluster.connect("couchbases://your-ip", ClusterOptions(PasswordAuthenticator("Administrator","password",cert_path="/path/to/cluster.crt")))
       #end::tls[]
 
       print("dnssrv")
+      # dns srv is enabled by default in the C++ core
       #tag::dnssrv[]
-      env = ClusterEnvironment.builder() \
-          .ioConfig(IoConfig.enableDnsSrv(true)) \
-          .build()
+      auth = PasswordAuthenticator("Administrator", "password")
+      opts = ClusterOptions(auth, enable_dns_srv=True)
       #end::dnssrv[]
 
 example = ManagingConnections()
